608f1a6cb2835e98c71c7865411c30bbee8c29fa
Chibuikem Amaechi <chibuikem_amaechi@Chibuikems-MacBook-Pro.local>
2017-12-08 11:26:59 -0600

    Modified main.cpp.swp and main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-//Program reads in an arbitrary amount of grades (0 - 5) from a class of students
-//and documents the number of times a certain grade was made.
+//Program reads in an arbitrary amount of grades (0 - 5) from a class of
+//students and documents the number of times a certain grade was made.
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
@@ -11,8 +11,8 @@ void input(int gradeValues[], int arraySize);
 //Postcondition: The array gradeValuesh has been filled;
 
 void documentQuizScore(int gradeValues[], int arraySize, int grade);
-//Precondition: All elements of the array gradeValues has been initialized to zero.
-//arraySize >= 1.
+//Precondition: All elements of the array gradeValues has been initialized to
+//zero. arraySize >= 1.
 //Postcondition: Increments a certain element of gradeValues that contains
 //a value equal to grade.
 
@@ -24,41 +24,41 @@ const int GRADE_SCALE = 6;
 
 int main()
 {
-	int gradeValues[GRADE_SCALE] = {0, 0, 0, 0, 0, 0};
-	
-	input(gradeValues, GRADE_SCALE);
-	displayQuizGradeHistogram(gradeValues, GRADE_SCALE);
+    int gradeValues[GRADE_SCALE] = {0, 0, 0, 0, 0, 0};
 
-	return 0;
+    input(gradeValues, GRADE_SCALE);
+    displayQuizGradeHistogram(gradeValues, GRADE_SCALE);
+
+    return 0;
 }
 
 void input(int gradeValues[], int arraySize)
 {
-	int grade;
+    int grade;
 
-	cout<<"Start entering grades: ";
+    cout<<"Start entering grades: ";
 
-	do
-	{
-		cin>>grade;
-		documentQuizScore(gradeValues, arraySize, grade);
-	}while(cin.peek() != '\n');
-	
-	cout<<endl;
+    do
+    {
+        cin>>grade;
+        documentQuizScore(gradeValues, arraySize, grade);
+    }while(cin.peek() != '\n');
+
+    cout<<endl;
 }
 
 void documentQuizScore(int gradeValues[], int arraySize, int grade)
 {
-	for (int i = 0; i < arraySize; i++)
-	{
-		if (grade == i)
-			++gradeValues[i];
-	}
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (grade == i)
+            ++gradeValues[i];
+    }
 }
 
 void displayQuizGradeHistogram(int gradeValues[], int arraySize)
 {
-	for (int i = 0; i < arraySize; i++)
-		cout<<gradeValues[i]<<" grade(s) of "<<i<<endl;
-	cout<<endl;
+    for (int i = 0; i < arraySize; i++)
+        cout<<gradeValues[i]<<" grade(s) of "<<i<<endl;
+    cout<<endl;
 }
